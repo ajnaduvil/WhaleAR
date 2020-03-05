2df800c73e871c9c8b7663f3851b886b73cd9366
ajnaduvil <ajnaduvil@gmail.com>
2020-03-05 23:14:13 +0530

    working on water vfx and timeline

---
+++ Assets/App/Scripts/UpdateSceneName.cs
@@ -0,0 +1,20 @@
+﻿using AVR.Utils.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Updates the changed scene name to text
+/// </summary>
+public class UpdateSceneName : MonoBehaviour
+{
+    public SceneSwitcher sceneSwitcher;
+    public Text sceneNameText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneSwitcher.sceneChanged += (sceneName) => { sceneNameText.text = sceneName; };
+    }
+
+}
